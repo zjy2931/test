242f374bdccda46e20caf838359abdecbf3a141a
zjy <479247526@qq.com>
2022-09-27 18:48:10 +0800

    init

--- minicode-1/index/index.js
+++ minicode-1/index/index.js
@@ -1,55 +1,29 @@
 Page({
   data: {
-    focus: false,
     content: [],
-    maxlength: [
-      -1,
-      -1,
-      -1,
-    ]
   },
   onLoad() {
     this._content = []
   },
   changeLength(e) {
-    console.log(e.detail.value)
-    const id = e.target.dataset.id
-    let arr = this.data.content.reduce((length, item, index) => {
-      if (id != index && item) {
-        length = length - item.length
-      }
-      return length
-    }, 10)
-    this.setData({
-      [`maxlength[${id}]`]: arr
-    })
-    console.log(arr)
+    this.changeValue(e)
   },
   changeValue(e) {
-    console.log(this.data.content.join(''))
     const id = e.target.dataset.id
     const value = e.detail.value
-    const total = this.data.content.join().length
+    const total = this.data.content.join("").length
     const preValue = this._content[id] || ''
-    console.log(total, preValue)
-    console.log()
-    if (total > 10) {
-      console.log("已经超过了")
-      if (value.length >= (this._content?.[id]?.length || '')) {
-        this.setData({
-          [`content[${e.target.dataset.id}]`]: this._content[id] || "",
-        })
-      } else {
-        this.setData({
-          [`content[${id}]`]: value,
-        })
-      }
-      return
-    } else {
-      this._content[id] = value
+    console.log(total,)
+    if (total < 10 || (value.length < (this._content?.[id]?.length || ''))) {
+      this.setData({
+        [`content[${id}]`]: value,
+      })
+      this._content[id] =value
+      return 
     }
     this.setData({
-      [`content[${id}]`]: value,
+      [`content[${e.target.dataset.id}]`]: preValue,
     })
+
   }
 })
